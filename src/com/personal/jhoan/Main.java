5412a4b3982aea2b4ab552ab1d9bfff8a950b67f
JHOAN S <jhoansgonzalez@outlook.com>
2023-06-26 19:14:50 -0500

    Repositorio

---
+++ src/com/personal/jhoan/Main.java
@@ -0,0 +1,39 @@
+package com.personal.jhoan;
+
+public class Main {
+    public static void main(String[] args) {
+// Tipos de datos
+        // Enteros
+        byte variablebyte= 5;
+        short variableshort= 5;
+        int variableint= 5;
+        long variablelong= 5;
+        System.out.println(
+                "Variable byte: " + variablebyte+
+                "   Variable short: " + variableshort+
+                "   Variable int: " + variableint+
+                "   Variable long: " + variablelong
+        );
+        // Decimalea
+        float variablefloat= 5.5f;
+        double variabledouble= 5.5D;
+        System.out.println(
+                "Variable float: " + variablefloat+
+                "   Variable double: " + variabledouble
+        );
+
+        // Booleano
+        boolean variableboolean= true;
+        System.out.println(
+                "Variable boolean: " + variableboolean
+        );
+        // Char
+        char variablechar= 'a';
+        String variablestring="Hola";
+        System.out.println(
+                "Variable char: " + variablechar+
+                "   Variable string: " + variablestring
+        );
+
+    }
+}
